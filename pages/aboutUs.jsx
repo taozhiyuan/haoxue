501823f55e79b167522433a012e4766cec228e84
taozhiyuan <330941110@qq.com>
2018-06-20 13:47:17 +0800

    修改了about页面轮播页面无法显示

--- pages/aboutUs.jsx
+++ pages/aboutUs.jsx
@@ -7,12 +7,32 @@ import Manager from '../components/aboutUs/Manager.jsx'
 import Business from '../components/aboutUs/Business.jsx'
 import Cooperation from '../components/aboutUs/Cooperation.jsx'
 import Map from '../components/aboutUs/Map.jsx'
+import axios from '../global/axios'
+import Error from 'next/error'
 
 export default class AboutUs extends Component {
+    static async getInitialProps(){
+        try{
+            const banner = await axios.getArticleList({ articleType : '1' });
+            return {
+                data : {
+                    bannerList : banner.data.result
+                },
+                status : 200
+            }
+        }catch(err) {
+            if(!err.response) return { status : 404 }
+            return { status : err.response.status  }
+        }
+    }
     render(){
+        const { data, status } = this.props;
+        if ( status !== 200 ) {
+            return <Error statusCode={ status } />
+        }
         return(
             <Layout>
-                <Banner />
+                <Banner data={ data.bannerList.list } />
                 {/* 广告栏 */}
                 <About />
                 {/* 关于我们 */}
